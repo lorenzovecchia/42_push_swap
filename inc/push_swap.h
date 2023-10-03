2a1223f65dd231ba11b315b439cbdf2a191f9867
Lore <lore@MacBook-Pro-de-Lore.local>
2023-10-03 21:00:24 +0200

    .h added

---
+++ inc/push_swap.h
@@ -0,0 +1,18 @@
+#ifndef PUSH_SWAP_H
+# define PUSH_SWAP_H
+
+# include "../libft/libft.h"
+# include <limits.h>
+# include <stdlib.h>
+# include <unistd.h>
+/*# include <string.h>
+# include <stdio.h>
+
+// ------------------------ Struct ------------------------ //
+typedef struct s_stack
+{
+	int				num;
+	struct s_stack	*next;
+}	t_stack;
+
+#endif
